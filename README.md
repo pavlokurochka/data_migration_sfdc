67ce4efef4c83a757b77f8e4fa17f1ecb470bfd2
Pavlo Kurochka <kurochka@gmail.com>
2024-04-25 18:48:37 -0700

    parameters and readme

--- README.md
+++ README.md
@@ -22,36 +22,192 @@ Extended Search> Advanced Search -Contractors> Unselect all Filters > Run Docume
 
 I saved all downloaded source files into the `data` folder: [src_mine_information.xlsx](data/src_mine_information.xlsx), [src_addresses_of_record.xlsx](data/src_addresses_of_record.xlsx), [src_operator_report.xlsx](data/src_operator_report.xlsx), and [src_contractors.xlsx](data/src_contractors.xlsx). We'd work with `src_contractors.xlsx` for now. I'll save the rest for the future exercises.
 
-## Getting Salesforce.com developer account
 
-[Sign up for your Salesforce Developer Edition](https://developer.salesforce.com/signup). Save your user name and password.
 
 ## Infrastructure
 
 ### Environment setup 
 
-I developed this project locally on my Windows 11 laptop. I kept the code OS agnostic. If you want to run it locally install Python (I recommend [Anaconda](https://www.anaconda.com/download/success) distribution) and [Visual Studio Code](https://code.visualstudio.com/download).
+I developed this project locally on my Windows 11 laptop. I kept the code OS agnostic. If you want to run it locally install Python (I recommend [Anaconda](https://www.anaconda.com/download/success) distribution), Visual Studio Code](https://code.visualstudio.com/download) and [git](https://git-scm.com/downloads). Then clone this repo and change dir to the project folder.
 
 Alternatively, you can run this code on GitHub [codespaces](https://github.com/codespaces): fork this repository to your GitHub account. Click on **Code** and then **Create codespace on main**.
 
- 
+ ![](pictures/open_codespaces.png)
 
-### Authentication and secret management
+We are using several Python modules that need to be installed: [duckdb](https://duckdb.org/docs/guides/python/install.html), [Simple Salesforce](https://pypi.org/project/simple-salesforce/) 
 
-## Data Migration Methodology via steps to reproduce this project 
+### TODO
+
+```bash
+pip install
+```
+
+
+
+## Data Migration Methodology via Steps to Reproduce This Project
+
+### Target and Source System Discovery  and Establishing Connectivity
+
+#### Getting Salesforce.com developer account
+
+[Sign up for your Salesforce Developer Edition](https://developer.salesforce.com/signup). Save your user name and password.
+
+#### Saleseforce.com Authentication
+
+Getting authentication to work with simple_salesforce in Python was a biggest hurdle for me in this project. We already have username and password that we saved when we got the developer account. Now the third part:`security_token`.
+
+You can get it in Salesforce.com, using `Lightning` Theme:
+
+Click in the top right corner your `profile icon` -> `Settings`
+
+![](pictures/sfdc_security_token1.png)
+
+In the tree select `My Personal Information` -> `Reset My Security Token`
+
+Click `Reset Security Token`
+
+![](pictures/sfdc_security_token2.png)
+
+Check your email for a new token.
+
+####  Secret Management
+
+In VS Code create `.env` file in the project directory.
+
+![](pictures/env_file.png)
+
+Paste following lines into it:
+
+```bash
+SFDC_USERNAME="REPLACE_SFDC_USERNAME"
+SFDC_PASSWORD="REPLACE_SFDC_PASSWORD
+SFDC_SECURITY_TOKEN="REPLACE_SFDC_SECURITY_TOKEN"
+```
+
+Paste your authentication values instead of placeholders.
 
 ### Source Tables Snapshot
 
+```bash
+python migrate2sfdc.py  --action get_src
+```
+
+```
+Data Migration into Salesforce.com
+Refreshing Source Tables Snapshot
+Read 89900 records from data\src_addresses_of_record.xlsx
+Read 87098 records from data\src_contractors.xlsx
+Read 90587 records from data\src_mine_information.xlsx
+Read 90196 records from data\src_operator_report.xlsx
+```
+
+
+
 ### Target Tables Snapshot
+
+```bash
+python migrate2sfdc.py  --action get_tgt
+```
+
+```
+Data Migration into Salesforce.com
+Refreshing Target Tables Snapshot
+Dowloaded 2340 records for Account 
+ saved into table tgt_account 
+ saved into file data\tgt_account.xlsx 
+Dowloaded 0 records for Address 
+Dowloaded 20 records for Contact 
+ saved into table tgt_contact 
+ saved into file data\tgt_contact.xlsx 
+```
+
+
+
+### Data Profiling
+
+```bash
+python migrate2sfdc.py  --action profile --src_table src_contractors
+```
+
+```
+Data Migration into Salesforce.com
+Profiling table {src_table}
+SUMMARIZE src_contractors
+| column_name        | column_type   | min                     | max             |   approx_unique |   count | null_percentage   |
+|:-------------------|:--------------|:------------------------|:----------------|----------------:|--------:|:------------------|
+| Contractor ID      | VARCHAR       | 073                     | ZZZ             |           87368 |   87098 | 0.0%              |
+| Contractor Name    | VARCHAR       | Beeson Welding          | zap enterprises |           84320 |   87098 | 0.0%              |
+| PO Box             | VARCHAR       | 10                      | Z               |            6517 |   87098 | 69.14%            |
+| Postal             | VARCHAR       | 08788                   | p0m1b0          |             167 |   87098 | 99.8%             |
+| City               | VARCHAR       | Albuquerque             | zephyrhills     |           15373 |   87098 | 0.0%              |
+| Street             | VARCHAR       | 5129 GILBERTSVILLE HWY. | w7022 hwy 11    |           57355 |   87098 | 31.21%            |
+| State Abbreviation | VARCHAR       | AK                      | WY              |              57 |   87098 | 0.21%             |
+| Zip Code           | DOUBLE        | 0.0                     | 998019115.0     |           20900 |   87098 | 0.22%             |
+```
 
 ### Column Mapping
 
+### 
+
+```bash
+python migrate2sfdc.py  --action get_map
+```
+
+```
+Data Migration into Salesforce.com
+Refreshing maping table
+Loaded 67 records from data\mapping.xlsx
+```
+
 ### Create or Refresh Staging Table
+
+```bash
+python migrate2sfdc.py  --action stage --obj_name Account --key_column AccountNumber --src_table src_contractors
+```
+
+```
+Data Migration into Salesforce.com
+Creating or Refreshing Staging Table
+Staging table stg_account is refreshed
+```
+
+### 
 
 ### Produce Pre-Load report
 
+```bash
+python migrate2sfdc.py  --action pre_load_create
+```
+
+```
+Creating Pre-load Reports
+Defined report preload_account
+```
+
+```bash
+python migrate2sfdc.py  --action pre_load_run
+```
+
+```
+Data Migration into Salesforce.com
+Running Pre-load Reports
+Created report data\preload_account.xlsx. 87098 records.
+```
+
+### 
+
 ### Load into Target System
+
+```bash
+python migrate2sfdc.py  --action load --obj_name Account --key_column AccountNumber --src_table src_contractors --batch_size 50
+```
+
+![](pictures/load_progress.png)
 
 ### Produce Post-Load Report
 
-### Summary
+
+
+## Summary
+
+![](pictures/storage_limits.png)
---
+++ migrate2sfdc.py
@@ -0,0 +1,346 @@
+# %%
+# pip install simple-salesforce
+# https://pypi.org/project/simple-salesforce/
+# https://salesforce.stackexchange.com/questions/120318/accessing-the-salesforce-api-for-the-first-time-using-python?newreg=0dfd87044b814f2c84ce2cd9ec5e0841
+
+# https://developer.salesforce.com/blogs/2021/09/how-to-automate-data-extraction-from-salesforce-using-python
+from simple_salesforce import Salesforce
+import pandas as pd
+import duckdb
+import os
+from dotenv import load_dotenv
+import glob
+from tqdm import tqdm
+import argparse
+
+# %%
+con = duckdb.connect("sfdc_dm.db")
+con.install_extension("spatial")
+con.load_extension("spatial")
+con.sql("SET TimeZone='America/Los_Angeles'")
+# %%
+load_dotenv()
+# %%
+sfdc_username = os.environ["SFDC_USERNAME"]
+sfdc_password = os.environ["SFDC_PASSWORD"]
+sfdc_security_token = os.environ["SFDC_SECURITY_TOKEN"]
+sf = Salesforce(
+    username=sfdc_username,
+    password=sfdc_password,
+    security_token=sfdc_security_token,
+    # domain="test", # don't include if logging into dev account
+)
+
+
+# %%
+def refresh_tgt_tables(tables: list | None = None):
+    if tables is None or len(tables) == 0:
+        tables = ["Account", "Address", "Contact"]
+    for obj_name in tables:
+        # obj_name = 'Account'
+        table_name = f"tgt_{obj_name.lower()}"
+        full_name = f"sf.{obj_name}"
+        tgt_object = eval(full_name)
+        names = []
+        for field in tgt_object.describe()["fields"]:
+            # print(field['name'])
+            names.append(field["name"])
+        # ','.join(names)
+        df = pd.DataFrame(
+            sf.query_all(f"""SELECT {','.join(names)} FROM {obj_name}""")["records"]
+        )
+        print(f"Dowloaded {df.shape[0]} records for {obj_name} ")
+        if df.shape[0] == 0:
+            continue
+        con.sql(f"DROP table IF EXISTS {table_name}")
+        con.sql(f"create table {table_name} as select * from df")
+        print(f" saved into table {table_name} ")
+        report_file_path = os.path.join("data", f"{table_name}.xlsx")
+        df.to_excel(report_file_path, index=False)
+        print(f" saved into file {report_file_path} ")
+
+
+# refresh_tgt_tables(['Account'])
+# refresh_tgt_tables([])
+
+
+# %%
+def refresh_src_tables():
+    file_mask = os.path.join("data", "src*.xlsx")
+    files = glob.glob(file_mask)
+    # print( files)
+    for source_file in files:
+        # print(os.path.basename(os.path.splitext(source_file)[0]) )
+        table_name = os.path.basename(os.path.splitext(source_file)[0]).lower()
+        df = pd.read_excel(io=source_file, header=2)
+        print(f"Read {df.shape[0]} records from {source_file}")
+        if df.shape[0] == 0:
+            continue
+        con.sql(f"DROP table IF EXISTS {table_name}")
+        con.sql(f"create table {table_name} as select * from df")
+
+
+# refresh_src_tables()
+# %%
+
+
+def load_mapping():
+    source_file = os.path.join("data", "mapping.xlsx")
+    mapp = con.sql(
+        f"""  SELECT * FROM st_read(
+                '{source_file}',
+                open_options=['HEADERS=FORCE', 'FIELD_TYPES=AUTO'])
+        """
+    )
+    con.sql("DROP table IF EXISTS mapping")
+    con.sql("create table mapping as select * from mapp")
+    print(f"Loaded {len(mapp)} records from {source_file}")
+
+
+# load_mapping()
+# %%
+def refresh_staging(
+    obj_name: str = "Account",
+    key_column: str = "AccountNumber",
+    src_table:str ="src_contractors",
+):
+    mapping_columns = con.sql(f"""SELECT 
+            CASE 
+            WHEN transform_action = 'copy' THEN concat(source_table,'."',source_column,'"')  
+            WHEN transform_action = 'default' THEN concat('''',default_value,'''')
+            WHEN transform_action = 'rule' THEN rule_expression  END col_content,
+            target_field col_alias
+            FROM mapping where transform_action IS NOT NULL
+                AND  target_object = '{obj_name}' 
+                AND  source_table = '{src_table}' 
+                """).df()
+    cols_ = []
+    for _i, row in mapping_columns.iterrows():
+        cols_.append(f"{row.col_content} AS {row.col_alias}")
+    cols_.append(f" '{src_table}' AS source_table")
+    stg_table = f"stg_{obj_name.lower()}"
+    # src_table = con.sql(f"""SELECT MAX(source_table) source_table FROM mapping where transform_action IS NOT NULL
+    #             AND  target_object = '{obj_name}'""").fetchall()[0][0]
+    select_from_source = f"SELECT {','.join(cols_)} FROM {src_table}"
+    if stg_table not in list(con.sql("SHOW ALL TABLES").df()["name"]):
+        con.sql(f"DROP table IF EXISTS {stg_table}")
+        con.sql(f"CREATE TABLE {stg_table} AS {select_from_source}")
+        con.sql(f"ALTER TABLE {stg_table} ADD COLUMN id VARCHAR")
+        con.sql(f"ALTER TABLE {stg_table} ADD COLUMN success BOOLEAN")
+        con.sql(f"ALTER TABLE {stg_table} ADD COLUMN errors VARCHAR[]")
+    else:
+        con.sql(f"DELETE FROM {stg_table} WHERE source_table = '{src_table}' ")
+        con.sql(f"INSERT INTO {stg_table} BY NAME {select_from_source}")
+    con.sql(f"""UPDATE {stg_table}  SET id = tgt_{obj_name.lower()}.id 
+            FROM tgt_{obj_name.lower()} 
+            WHERE
+            {stg_table}.{key_column} = tgt_{obj_name.lower()}.{key_column} """)
+    if obj_name == "Account":
+        stg_account_errors()
+    print(f"Staging table {stg_table} is refreshed")
+
+
+# refresh_staging()
+
+
+# %%
+def load_in_sfdc(
+    obj_name: str = "Account",
+    key_column: str = "AccountNumber",
+    src_table: str = "src_contractors",
+    batch_size: int = 10,
+):
+    # key_column = "AccountNumber"
+    # obj_name = "Account"
+    # src_table = "src_contractors"
+    stg_table = f"stg_{obj_name.lower()}"
+    # batch_size = 100
+    sql = f"""SELECT * EXCLUDE (id, success, errors, source_table) 
+            FROM {stg_table} 
+            WHERE id IS NULL AND source_table ='{src_table}'
+            AND errors IS NULL
+            LIMIT {batch_size}"""
+    # print(sql)
+    payload = con.sql(sql).df().to_dict(orient="records")
+
+    for a in tqdm(payload, desc=f"Loading {obj_name} into Salesforce.com"):
+        # print(a)
+        try:
+            result = sf.Account.create(a)
+            # print(result)
+            update_sql = f"""UPDATE {stg_table} SET id = '{result['id']}' 
+                , success = '{result['success']}'   
+                , errors = '{result['errors']}' 
+                WHERE {key_column} = '{a[key_column]}'  
+                """
+        except Exception as err:
+            # print(type(err))
+            update_sql = f"""UPDATE {stg_table} SET   success = 'False'   
+                , errors = ['{str(type(err)).replace("'",'')}' ]
+                WHERE {key_column} = '{a[key_column]}'  
+                """
+            # print(update_sql)
+
+        con.sql(update_sql)
+
+
+# load_in_sfdc()
+
+
+# %%
+def create_preload_reports():
+    con.sql("""CREATE OR REPLACE VIEW preload_account as SELECT * from stg_account""")
+    print("Defined report preload_account")
+
+# create_preload_reports()
+# %%
+def run_preload_reports():
+    for view_name in list(
+        con.sql("""SELECT * from information_schema.tables 
+                WHERE table_type ='VIEW'""").df()["table_name"]
+    ):
+        report_file_path = os.path.join("data", f"{view_name}.xlsx")
+        if os.path.exists(report_file_path):
+            os.remove(report_file_path)
+        # con.sql(
+        #     f"""COPY (SELECT * from {view_name})
+        #     TO '{report_file_path}'
+        #     WITH (FORMAT GDAL, DRIVER 'xlsx') """
+        # )
+        df = con.sql(f"""SELECT * from {view_name}""").df()
+        df.to_excel(report_file_path, index=False)
+        print(f"Created report {report_file_path}. {df.shape[0]} records.")
+
+
+# %%
+def stg_account_errors():
+    """Mark as errors all accounts with duplicate names"""
+    sql = """update stg_account set errors = [dups.dup_number::varchar]
+            FROM
+            (with same_names as (
+            SELECT
+                Name
+            FROM
+                stg_account
+            group by
+                all
+            HAVING
+                count(1)>1) 
+            select
+                ss.Name,
+                ss.AccountNumber,
+                ROW_NUMBER() over( PARTITION by ss.Name
+            order by ss.id desc,
+                ss.AccountNumber) dup_number
+            from
+                stg_account ss
+            join same_names sn on
+                ss.Name = sn.Name) dups
+            where dups.AccountNumber = stg_account.AccountNumber
+            and dups.dup_number >1 """
+    con.sql(sql)
+
+
+# %%
+def profile_db_table(table_name: str = "src_contractors"):
+    sql = f"SUMMARIZE {table_name}"
+    print(sql)
+    print(
+        con.sql(sql)
+        .df()[
+            [
+                "column_name",
+                "column_type",
+                "min",
+                "max",
+                "approx_unique",
+                "count",
+                "null_percentage",
+            ]
+        ]
+        .to_markdown(index=False)
+    )
+
+
+# %%
+if __name__ == "__main__":
+    print("Data Migration into Salesforce.com")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--action",
+        type=str,
+        choices=[
+            "get_src",
+            "get_tgt",
+            "profile",
+            "get_map",
+            "stage",
+            "load",
+            "pre_load_create",
+            "pre_load_run",
+            "post_load_create",
+            "post_load_run",
+        ],
+        default="get_tgt",
+        help="Enter action ",
+        # required = True
+    )
+
+    parser.add_argument(
+        "--obj_name",
+        type=ascii,
+        default="Account",
+    )
+    parser.add_argument(
+        "--key_column",
+        type=ascii,
+        default="AccountNumber",
+    )
+    parser.add_argument(
+        "--src_table",
+        type=ascii,
+        default="src_contractors",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=10,
+    )
+
+    args = parser.parse_args()
+    # print(args)
+    obj_name = eval(args.obj_name)
+    key_column = eval(args.key_column)
+    src_table = eval(args.src_table)
+    if args.action == "get_src":
+        print("Refreshing Source Tables Snapshot")
+        refresh_src_tables()
+    if args.action == "get_tgt":
+        print("Refreshing Target Tables Snapshot")
+        refresh_tgt_tables()
+    if args.action == "profile":
+        print("Profiling table {src_table}")
+        profile_db_table(src_table)
+    if args.action == "get_map":
+        print("Refreshing maping table")
+        load_mapping()
+    if args.action == "get_map":
+        print("Refreshing maping table")
+        load_mapping()
+    if args.action == "stage":
+        print("Creating or Refreshing Staging Table")
+        refresh_staging(obj_name=obj_name, key_column=key_column, src_table=src_table)
+    if args.action == "pre_load_create":
+        print("Creating Pre-load Reports")
+        create_preload_reports()
+    if args.action == "pre_load_run":
+        print("Running Pre-load Reports")
+        run_preload_reports()
+    if args.action == "load":
+        print(f"Strting the load: {vars(args)} ")
+        load_in_sfdc(obj_name=obj_name, key_column=key_column, src_table=src_table, batch_size = args.batch_size)
+
+
+# %%
+con.close()
